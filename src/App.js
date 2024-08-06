5a2fed765e5dd849ea25c6394d6e1d1c7c683e98
Mahbubur Rahman <durlovrahman32@gmail.com>
2024-08-06 19:36:39 +0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import AppointmentScheduler from './components/AppointmentScheduler';
+import CalendarView from './components/CalendarView';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="container">
+        <nav>
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+          <a href="/scheduler">Scheduler</a>
+          <a href="/appointments">Appointments</a>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/scheduler" element={<AppointmentScheduler />} />
+          <Route path="/appointments" element={<CalendarView />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
