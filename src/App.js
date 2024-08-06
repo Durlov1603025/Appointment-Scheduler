dfe8ff95d08329eff30d2756529e0f2c6e997378
Mahbubur Rahman <durlovrahman32@gmail.com>
2024-08-07 00:46:17 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import AppointmentScheduler from './components/AppointmentScheduler';
 import CalendarView from './components/CalendarView';
+import UserList from './components/UserList';
 import './App.css';
 
 const App = () => {
@@ -15,12 +16,14 @@ const App = () => {
           <a href="/register">Register</a>
           <a href="/scheduler">Scheduler</a>
           <a href="/appointments">Appointments</a>
+          <a href='/users'>UserLists</a>
         </nav>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/scheduler" element={<AppointmentScheduler />} />
           <Route path="/appointments" element={<CalendarView />} />
+          <Route path='/users' element={<UserList />} />
         </Routes>
       </div>
     </Router>
